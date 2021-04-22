aabe794b25607f311c260b946a015b65fe769b9d
A-Wakidou <57793874+A-Wakidou@users.noreply.github.com>
2021-04-22 14:05:27 +0200

    adding marker title

--- Screens/Map.js
+++ Screens/Map.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import MapView, {Marker} from 'react-native-maps';
 import { StyleSheet, View, Dimensions } from 'react-native';
 
 
 export default function Map({users}) {
+  console.log(users)
   return (
     <View style={styles.container}>
       <MapView style={styles.map}>
@@ -15,8 +16,7 @@ export default function Map({users}) {
           longitude: user.address.geo.lng,
           }}
           image={require('../assets/marker.png')}
-          title="User1"
-          description="Description1"
+          title= {user.name}
           />
         ))}
       </MapView>
